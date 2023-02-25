3558acbee9d8d798f91e73c82737d1f702aa331a
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-02-25 00:44:57 -0800

    Update QuoteClient.java

--- Project1 - Warm up of Interprocess Communication/Q1/QuoteClient.java
+++ Project1 - Warm up of Interprocess Communication/Q1/QuoteClient.java
@@ -8,5 +8,20 @@ import java.io.*;
 
 public class QuoteClient
 {
-	//Your code is here
+	public static void main(String[] args){
+		try{
+	  		Socket sock = new Socket("localhost", 6017);
+			InputStream in = sock.getInputStream();	
+			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+			// read the date from the socket
+
+			String line;
+			while ((line = bin.readLine()) != null)
+				System.out.println(line);
+			// close the socket
+			sock.close();
+		}catch(IOException ioe){
+			System.err.println(ioe);
+		}
+	}
 }
