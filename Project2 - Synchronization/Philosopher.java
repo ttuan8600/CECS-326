fabc04df0c3f1afea8e1c6db9f92981afa2c50f6
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-03-15 17:59:04 -0700

    Update Philosopher.java

--- Project2 - Synchronization/Philosopher.java
+++ Project2 - Synchronization/Philosopher.java
@@ -5,46 +5,50 @@
  * Philosophers alternate between eating and thinking.
  */
 
+// Needed to mnake the sleep time random from 1s to 3s
+import java.util.Random;
+
 public class Philosopher implements Runnable
 {
     private int philNumber;
-    private DiningServer server;
+    private DiningServer dServer;
+    private Random rand;
     
     public Philosopher(int philNumber, DiningServer server)
     {
         this.philNumber = philNumber;
-        this.server = server;
+        this.dServer = server;
+        this.rand = new Random();
     }
     
     public void run()
     {
-        while (true)
-        {
-            // Think for a while
-            try
-            {
-                Thread.sleep((int)(Math.random() * 1000));
+        try{
+            while (true){
+                // Think for a while, and take a fork
+                think();
+                dServer.takeForks(philNumber);
+                // Eat for a while, then return a fork
+                eat();
+                dServer.returnForks(philNumber);
             }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-            
-            // Try to get forks
-            server.takeForks(philNumber);
-            
-            // Eat for a while
-            try
-            {
-                Thread.sleep((int)(Math.random() * 1000));
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-            
-            // Return forks
-            server.returnForks(philNumber);
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            return;
         }
+    }
+
+    private void think() throws InterruptedException
+    {
+        int sleepTime = rand.nextInt(2000) + 1000;
+        System.out.println("Philosopher #" + philNumber + " is thinking for " + sleepTime + "ms");
+        Thread.sleep(sleepTime);
+    }
+
+    private void eat() throws InterruptedException
+    {
+        int sleepTime = rand.nextInt(2000) + 1000;
+        System.out.println("Philosopher #" + philNumber + " is eating for " + sleepTime + "ms");
+        Thread.sleep(sleepTime);
     }
 }
