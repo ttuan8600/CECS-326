6f3bb0452a12041638431378fed67ac873f2d0ae
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-03-16 21:39:43 -0700

    update

---
+++ Project2 - Synchronization/CECS 326 Project 2/Philosopher.java
@@ -0,0 +1,57 @@
+/*
+ * Philosopher.java
+ *
+ * This class represents each philosopher thread.
+ * Philosophers alternate between eating and thinking.
+ */
+
+// Needed to mnake the sleep time random from 1s to 3s
+import java.util.Random;
+
+public class Philosopher implements Runnable
+{
+    private int philNumber;
+    private DiningServer dServer;
+    private Random rand;
+    
+    public Philosopher(int philNumber, DiningServer server)
+    {
+        this.philNumber = philNumber;
+        this.dServer = server;
+        this.rand = new Random();
+    }
+    
+    public void run()
+    {
+        try{
+            while (true){
+                // Take forks
+                dServer.takeForks(philNumber);
+                Thread.sleep(rand.nextInt(2000) + 1000);
+                System.out.println("Forks are with Philosopher #" + philNumber);
+                // Eating
+                eat();
+                // Return forks
+                dServer.returnForks(philNumber);
+                // Thinking
+                think();
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void think() throws InterruptedException
+    {
+        int sleepTime = rand.nextInt(2000) + 1000;
+        System.out.println("Philosopher #" + philNumber + " took " + sleepTime + "ms thinking");
+        Thread.sleep(sleepTime);
+    }
+
+    private void eat() throws InterruptedException
+    {
+        int sleepTime = rand.nextInt(2000) + 1000;
+        System.out.println("Philosopher #" + philNumber + " took " + sleepTime + "ms eating");
+        Thread.sleep(sleepTime);
+    }
+}
---
+++ Project2 - Synchronization/CECS 326 Project 2/README.md
@@ -0,0 +1,30 @@
+### To run the Dining Philosophers problem program, follow these steps:
+
+1. Install Java on your machine if it is not already installed. You can download it from the official Java website.
+
+2. Create a new directory on your machine where you will store the program files.
+
+3. Extrac the four Java files (DiningServer.java, DiningServerImpl.java, Philosophers.java, and DiningPhilosophers.java) into the new directory.
+
+4. Open a terminal or command prompt and navigate to the directory where you saved the program files.
+
+5. Compile the program by running the following command:
+
+```ruby
+javac DiningServer.java 
+javac DiningServerImpl.java 
+javac Philosopher.java 
+javac DiningPhilosophers.java
+```
+6. Run the program by executing the following command:
+
+```ruby
+java DiningPhilosophers
+```
+This will start the program and you should see output in the terminal indicating the progress of the program.
+
+## Contributors
+
+Twan Tran
+
+Bharath Varma Kakarlapudi
