911d6457c36877e8c207e881de3ade8a1654392e
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-02 19:24:33 -0700

    changes

--- Project3 - All about Deadlock/RoadController.java
+++ Project3 - All about Deadlock/RoadController.java
@@ -3,15 +3,20 @@
  *
  */
 
- import java.util.concurrent.Semaphore;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
  
 public class RoadController
 {  
-   private Semaphore eastSemaphore = new Semaphore(1);
-   private Semaphore westSemaphore = new Semaphore(1);
+   private Semaphore road = new Semaphore(1);
+   private Lock eastVillageMutex = new ReentrantLock();
+   private Lock westVillageMutex = new ReentrantLock();
+   private int currentPriority = 0; // 0: East_village, 1: West_village
 
    public void requestAccessFromEast() throws InterruptedException
    {  
+      
       // Acquire the semaphores
       eastSemaphore.acquire();
       westSemaphore.acquire();
