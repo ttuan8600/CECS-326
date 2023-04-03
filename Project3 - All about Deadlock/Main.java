97a1e1ef81f8934a9ab3f0ad8cc1c7b0637561b6
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-03 05:30:34 -0700

    Update Main.java

--- Project3 - All about Deadlock/Main.java
+++ Project3 - All about Deadlock/Main.java
@@ -1,9 +1,23 @@
-public class main {
+
+import java.util.concurrent.Semaphore;;
+
+public class Main {
     public static void main(String[] args) {
-        RoadController roadController =  new RoadController();
-        for (int i = 0; i < 5; i++){
-            new East_village(roadController).start();
-            new West_village(roadController).start();
-        }
+        Semaphore roadSemaphore = new Semaphore(1);
+        Semaphore westMutex = new Semaphore(1);
+        Semaphore eastMutex = new Semaphore(0);
+
+        Thread eastVillage1 = new Thread(new East_village(roadSemaphore, eastMutex, westMutex, "East_village_1"));
+        Thread eastVillage2 = new Thread(new EastVillage(roadSemaphore, eastMutex, westMutex, "East_village_2"));
+        Thread eastVillage3 = new Thread(new EastVillage(roadSemaphore, eastMutex, westMutex, "East_village_3"));
+
+        Thread westVillage1 = new Thread(new West_village(roadSemaphore, westMutex, eastMutex, "West_village_1"));
+        Thread westVillage2 = new Thread(new WestVillage(roadSemaphore, westMutex, eastMutex, "West_village_2"));
+
+        eastVillage1.start();
+        eastVillage2.start();
+        eastVillage3.start();
+        westVillage1.start();
+        westVillage2.start();
     }
 }
