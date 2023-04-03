dd5ea577c15e58447c98e7e83d416ab4039fda93
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-02 17:31:57 -0700

    first try

--- Project3 - All about Deadlock/East_village.java
+++ Project3 - All about Deadlock/East_village.java
@@ -1,6 +1,29 @@
+import java.util.Random;
 
-public class East_village 
+public class East_village extends Thread
 {
-   //Your code here
-   
+   private RoadController roadController;
+
+   public East_village(RoadController roadController){
+      this.roadController = roadController;
+   }
+
+   public void run()
+   {
+      Random rand = new Random();
+      while(true){
+         try{
+            // Sleep for random time representing traveling
+            Thread.sleep(rand.nextInt(1000));
+            // Request access to the road
+            roadController.requestAccessFromEast();
+            // Sleep for random time reprensting eating
+            Thread.sleep(rand.nextInt(1000));
+            // Release access to the road
+            roadController.releaseAccessFromEast();
+         }catch(InterruptedException e){
+            e.printStackTrace();
+         }
+      }
+   }   
 }
