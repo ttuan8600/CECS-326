2d4e694c4a3d5b8515bd20de3c96963f7c0c1cbb
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-18 23:07:03 -0700

    Update schedule_fcfs.c

--- Project4 - CPU scheduler/schedule_fcfs.c
+++ Project4 - CPU scheduler/schedule_fcfs.c
@@ -10,4 +10,66 @@
 #include "cpu.h"
 
 struct node *head = NULL;
+struct node *tail = NULL;
+struct node *new = NULL;
+int num = 0;
+
+void add (char *name, int prior, int burst){
+    if (head == NULL){
+        head = malloc(sizeof(struct node));
+        tail = malloc(sizeof(struct node));
+
+        // set the name of the task
+        head->task = malloc(sizeof(struct task));
+        head->task->name = name;
+        head->task->priority = prior;
+        head->task->burst = burst;
+
+        // set the next node to be null
+        head->next = NULL;
+
+        // set the tail to be the head
+        tail = head;
+    }else{
+        // create a new node
+        new = malloc(sizeof(struct node));
+
+        tail->next = new;
+
+        // set the name of the task
+        new->task = malloc(sizeof(struct task));
+        new->task->name = name;
+        new->task->priority = prior;
+        new->task->burst = burst;
+
+        // set the next node to be null
+        new->next = NULL;
+
+        // set the tail to be the new node
+        tail = new;
+    }
+}
+
+void schedule(){
+    int final = 0;
+    float turnAround = 0;
+    float wait = 0;
+    float response = 0;
+
+    struct node *ref = head;
+    while (ref != NULL){
+        num += 1;
+        run(ref->task, ref->task->burst);
+        final += ref->task->burst;
+        turnAround += final;
+        if (ref->next != NULL){
+            response += 1;
+        }
+        wait = turnAround - final;
+
+        printf("The average turnaround time is : %f time units \n" , turnaroundtime/num);    
+        printf("The average ResponseTime is : %f time units \n" , ResponseTime/num);
+        printf("The average WaitTime is : %f time units\n " , WaitTime/num);
+    }
+}
 
