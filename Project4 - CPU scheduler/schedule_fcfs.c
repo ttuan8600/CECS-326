b54935656a3e87c67196130da48399b0b663170f
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-04-21 18:07:58 -0700

    Update schedule_fcfs.c

--- Project4 - CPU scheduler/schedule_fcfs.c
+++ Project4 - CPU scheduler/schedule_fcfs.c
@@ -11,42 +11,30 @@
 
 struct node *head = NULL;
 struct node *tail = NULL;
-struct node *new = NULL;
 
 void add(char *name, int prior, int burst)
 {
+    struct node *new = malloc(sizeof(struct node));
+
+    // set the name of the task
+    new->task = malloc(sizeof(struct task));
+    new->task->name = name;
+    new->task->priority = prior;
+    new->task->burst = burst;
+
+    // set the next node to be null
+    new->next = NULL;
+
     if (head == NULL)
     {
-        head = malloc(sizeof(struct node));
-        tail = malloc(sizeof(struct node));
-
-        // set the name of the task
-        head->task = malloc(sizeof(struct task));
-        head->task->name = name;
-        head->task->priority = prior;
-        head->task->burst = burst;
-
-        // set the next node to be null
-        head->next = NULL;
-
-        // set the tail to be the head
-        tail = head;
+        // set the head and tail to be the new node
+        head = new;
+        tail = new;
     }
     else
     {
-        // create a new node
-        new = malloc(sizeof(struct node));
-
+        // set the tail's next node to be the new node
         tail->next = new;
-
-        // set the name of the task
-        new->task = malloc(sizeof(struct task));
-        new->task->name = name;
-        new->task->priority = prior;
-        new->task->burst = burst;
-
-        // set the next node to be null
-        new->next = NULL;
 
         // set the tail to be the new node
         tail = new;
